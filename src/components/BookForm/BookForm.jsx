3000832d8688d652ac4d8f82798fde32f76ead16
Syimyk <syjmyk907@gmail.com>
2024-05-31 12:51:57 +0600

    hello

--- src/components/BookForm/BookForm.jsx
+++ src/components/BookForm/BookForm.jsx
@@ -6,67 +6,67 @@ import { setError } from "../../redux/slices/errorSlice";
 import { useDispatch } from "react-redux";
 
 const BookForm = ({ onSubmit }) => {
-  const [title, setTitle] = useState("");
-  const [author, setAuthor] = useState("");
-  const dispatch = useDispatch();
+	const [title, setTitle] = useState("");
+	const [author, setAuthor] = useState("");
+	const dispatch = useDispatch();
+	console.log(author, "hello world");
+	const handleSubmit = (e) => {
+		e.preventDefault();
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
+		if (title && author) {
+			const newBookObject = {
+				title: title,
+				author: author,
+				id: uuidv4(),
+			};
+			onSubmit(newBookObject);
+			setTitle("");
+			setAuthor("");
+		} else {
+			dispatch(setError("You must fill title and author!"));
+		}
+	};
 
-    if (title && author) {
-      const newBookObject = {
-        title: title,
-        author: author,
-        id: uuidv4(),
-      };
-      onSubmit(newBookObject);
-      setTitle("");
-      setAuthor("");
-    } else {
-      dispatch(setError("You must fill title and author!"));
-    }
-  };
-
-  return (
-    <div className="app-block book-form">
-      <h2>Add a New Book</h2>
-      <form onSubmit={handleSubmit}>
-        <div>
-          <label htmlFor="title">Title: </label>
-          <input
-            type="text"
-            id="title"
-            value={title}
-            onChange={(e) => setTitle(e.target.value)}
-          />
-        </div>
-        <div>
-          <label htmlFor="author">Author: </label>
-          <input
-            type="text"
-            id="author"
-            value={author}
-            onChange={(e) => setAuthor(e.target.value)}
-          />
-        </div>
-        <button type="submit">Add Book</button>
-        {/* <button type="button" onClick={handleAddRandomBook}>
+	return (
+		<div className="app-block book-form">
+			<h2>Add a New Book</h2>
+			<form onSubmit={handleSubmit}>
+				<div>
+					<label htmlFor="title">Title: </label>
+					<input
+						type="text"
+						id="title"
+						value={title}
+						onChange={(e) => setTitle(e.target.value)}
+					/>
+				</div>
+				<div>
+					<label htmlFor="author">Author: </label>
+					<input
+						type="text"
+						id="author"
+						value={author}
+						onChange={(e) => setAuthor(e.target.value)}
+					/>
+				</div>
+				<button type="submit">Add Book</button>
+				{/* <button type="button" onClick={handleAddRandomBook}>
           Add Random
         </button> */}
 
-        <button type="button">
-          {false ? (
-            <>
-              <span>Loading Book...</span>
-              <FaSpinner className="spinner" />
-            </>
-          ) : (
-            "Add Random via API"
-          )}
-        </button>
-      </form>
-    </div>
-  );
+				<button type="button">
+					{false ? (
+						<>
+							<span>Loading Book...</span>
+							<FaSpinner className="spinner" />
+						</>
+					) : (
+						"Add Random via API"
+					)}
+				</button>
+			</form>
+		</div>
+	);
 };
 
 export default BookForm;
